5925ef9536e6e9fd7a4916de1cb9005efcf60ab6
Intan Kusuma R <61036923+S1A4C1D2@users.noreply.github.com>
2020-04-10 20:50:34 +0700

    Update soal4b.c

--- soal4/soal4b.c
+++ soal4/soal4b.c
@@ -8,75 +8,65 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 
-typedef struct pass{
-	int i;
-} pass;
-
-int k=0, input;
-int number[21],matrix[21];
-int factorial=0,b=0,d=0;
-
-void* fungsi(void *arg);
+void* fungsi(void *arg)
+{
+	int angka=*(int*)arg;
+    int factorial=0;
+    int i,j,c;
+    for(i=0; i<=angka; i++)
+	{   
+	    factorial+=i;
+	}
+    int *p=(int*)malloc(sizeof(int));
+	*p=factorial;
+    
+	pthread_exit(p);
+}
 
 int main(void)
 {
 	key_t key=1412;
-	int shmid=shmget(key,sizeof(int)*4*5, IPC_CREAT | 0666);
+	int shmid=shmget(key,sizeof(int), IPC_CREAT | 0666);
 	int* res=(int*)shmat(shmid,NULL,0);
 	
-	int i, j;
+	int hasil[20];
+	int i;
+
+	for(i=0; i<20; i++)
+		hasil[i]=res[i];
+
     printf("Matriks hasil perkalian\n");
-	for(i=0; i<4; i++){
-		for(j=0; j<5; j++){
-			printf("%d ", res[i*5+j]);
-			number[i*5+j]=res[i*5+j];
-		}
+	for(i=0; i<20; i++){
+		printf("%d ", hasil[i]);
+		if((i+1)%5==0)
 		printf("\n");
 	}
-	printf("\n");
+	
+	pthread_t tid[20];
+	int j=0, x;
 
     int err;
     for (i=0;i<20;i++)
     {
-    	pass *arg = (pass*) malloc(sizeof(pass));
-            arg->i=i;
-           
-
-            pthread_t tid;
-			err = pthread_create(&tid,NULL,fungsi,(void*) arg);
-			if(err!=0)
-				printf("\n can't create thread : [%s]",strerror(err));
-			// else
-				// printf("\n create thread success\n");
-
-			pthread_join(tid,NULL);
+    	int *arg = (int*) malloc(sizeof(int));
+		arg=&hasil[i];
+		err = pthread_create(&tid[j++],NULL,fungsi,(void*) arg);
+		if(err!=0)
+			printf("\n can't create thread : [%s]",strerror(err));
+		// else
+			// printf("\n create thread success\n");
     }
 
     printf("Hasil penjumlahan\n");
-    for (i=0;i<4;i++){
-        for(j=0;j<5;j++)
-        {
-            printf("%d ", matrix[i*5+j]);
-        }
-        printf("\n");
-    }
+	for(i=0; i<20; i++){
+		void* k;
+		pthread_join(tid[i], &k);
+		int* p=(int*)k;
+		printf("%d ", *p);
+		if((i+1)%5==0)
+			printf("\n");
+	}
+
     return 0;
 }
 
-void* fungsi(void *arg)
-{
-	pass* angka=arg;
-    pthread_t id=pthread_self();
-    int i,j,c;
-    for(i=0; i<20; i++){
-	    for(j=0;j<=number[b];j++)
-	    {   
-	        factorial+=j;
-	    }
-    matrix[d]=factorial;
-    factorial=0;
-    b+=1;
-    d+=1;
-	}
-    return NULL;
-}
